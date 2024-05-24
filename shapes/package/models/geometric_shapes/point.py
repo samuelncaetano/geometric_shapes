191ed8c32d9dacd6a4525cda86e684256c3f7983
samuelncaetano <samueln.caetano42@gmail.com>
2024-05-24 16:46:31 -0300

    feat(point): create the point class

--- shapes/package/models/geometric_shapes/point.py
+++ shapes/package/models/geometric_shapes/point.py
@@ -1,3 +1,16 @@
-class Point:
-    def __init__(self):
-        pass
+from shapes.package.models.geometric_shape import GeometricShape
+
+
+class Point(GeometricShape):
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def calculate_area(self):
+        return 0
