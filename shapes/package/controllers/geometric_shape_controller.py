8af25bb35da5acf83bdce8ccd23f811cc5ddc9f0
samuelncaetano <samueln.caetano42@gmail.com>
2024-05-25 17:01:11 -0300

    feat(controller): add the memory repository to the controller

--- shapes/package/controllers/geometric_shape_controller.py
+++ shapes/package/controllers/geometric_shape_controller.py
@@ -3,64 +3,64 @@ from shapes.package.models import Point, LineSegment
 
 
 class GeometricShapeController:
-    def __init__(self):
-        self.__geometric_shapes = []
+    def __init__(self, repository):
+        self.__repository = repository
 
     def adicionar_forma_geometrica(self, forma_geometrica):
-        self.__geometric_shapes.append(forma_geometrica)
+        self.__repository.add(forma_geometrica)
 
     def listar_formas_geometricas(self):
-        if not self.__geometric_shapes:
+        formas = self.__repository.list_all()
+        if not formas:
             print("Nenhuma forma geométrica cadastrada.")
+            return
 
         print("### Formas Geométricas ###")
-        for i, forma in enumerate(self.__geometric_shapes, start=1):
+        for i, forma in enumerate(formas, start=1):
             print(f"{i}. {forma}")
 
-    # Usar os métodos das classes no controller
     def calcular_area(self, index):
-        if 0 <= index < len(self.__geometric_shapes):
-            forma = self.__geometric_shapes[index]
-            area = forma.calcular_area()
-            return area
+        forma = self.__repository.get(index)
+        if forma:
+            return forma.calcular_area()
+        return None
 
     def calcular_perimetro(self, index):
-        if 0 <= index < len(self.__geometric_shapes):
-            forma = self.__geometric_shapes[index]
-            perimetro = forma.calcular_perimetro()
-            return perimetro
+        forma = self.__repository.get(index)
+        if forma:
+            return forma.calcular_perimetro()
+        return None
 
     def distancia_origem(self, index):
-        if 0 <= index < len(self.__geometric_shapes):
-            forma = self.__geometric_shapes[index]
-            distancia_origem = forma.distancia_origem()
-            return distancia_origem
+        forma = self.__repository.get(index)
+        if forma:
+            return forma.distancia_origem()
+        return None
 
     def distancia_pontos(self, index, ponto):
-        if 0 <= index < len(self.__geometric_shapes):
-            forma = self.__geometric_shapes[index]
-            distancia_pontos = forma.distancia_pontos(ponto)
-            return distancia_pontos
+        forma = self.__repository.get(index)
+        if forma:
+            return forma.distancia_pontos(ponto)
+        return None
 
     def contem_ponto(self, index, ponto):
-        if 0 <= index < len(self.__geometric_shapes):
-            forma = self.__geometric_shapes[index]
-            resultado = forma.contem_ponto(ponto)
-            return resultado
+        forma = self.__repository.get(index)
+        if forma:
+            return forma.contem_ponto(ponto)
+        return None
 
     def mover_formas(self, index, novo_ponto):
-        if 0 <= index < len(self.__geometric_shapes):
-            forma = self.__geometric_shapes[index]
-            resultado = forma.mover(novo_ponto)
-            return resultado
+        forma = self.__repository.get(index)
+        if forma:
+            return forma.mover(novo_ponto)
+        return None
 
     def mover_segmento_de_reta(self, index, novo_ponto1, novo_ponto2):
-        if 0 <= index < len(self.__geometric_shapes):
-            forma = self.__geometric_shapes[index]
-            resultado = forma.mover(novo_ponto1, novo_ponto2)
-            return resultado
+        forma = self.__repository.get(index)
+        if forma:
+            return forma.mover(novo_ponto1, novo_ponto2)
+        return None
 
-    # Adicionar formas no controller
     def adicionar_ponto(self):
         ponto = Point.criar_ponto()
         self.adicionar_forma_geometrica(ponto)
