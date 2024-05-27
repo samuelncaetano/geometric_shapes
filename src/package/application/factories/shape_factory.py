1c0614fd783a3d7e5faa8b59587a3219633586dd
samuelncaetano <samueln.caetano42@gmail.com>
2024-05-27 19:05:43 -0300

    feat(factories): create factories for geometric shapes

---
+++ src/package/application/factories/shape_factory.py
@@ -0,0 +1,18 @@
+from src.package.domain import Point, LineSegment, Circle, Rectangle, Triangle
+
+
+class ShapeFactory:
+    @staticmethod
+    def criar_forma(tipo_forma):
+        if tipo_forma == "ponto":
+            return Point.criar_ponto()
+        elif tipo_forma == "segmento_de_reta":
+            return LineSegment.criar_segmento_de_reta()
+        elif tipo_forma == "circulo":
+            return Circle.criar_circulo()
+        elif tipo_forma == "retangulo":
+            return Rectangle.criar_retangulo()
+        elif tipo_forma == "triangulo":
+            return Triangle.criar_triangulo()
+        else:
+            raise ValueError("Tipo de forma desconhecido.")
