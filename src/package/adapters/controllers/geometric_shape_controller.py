7797184dbdb070dfd4b5bd04e28a70634333a2f2
samuelncaetano <samueln.caetano42@gmail.com>
2024-06-11 13:48:27 -0300

    refactor(controller): Adapt the controller to the architecture

--- src/package/adapters/controllers/geometric_shape_controller.py
+++ src/package/adapters/controllers/geometric_shape_controller.py
@@ -1,75 +1,51 @@
 # pylint: disable=R1710
-from src.package.domain import Point, LineSegment, Circle, Rectangle, Triangle
+from src.package import CalculateMetrics, CreateShape, MoveShape
 
 
 class GeometricShapeController:
-    def __init__(self, repository):
-        self.__repository = repository
+    def __init__(self, repository, factory):
+        self.create_shape_use_case = CreateShape(repository, factory)
+        self.move_shape_use_case = MoveShape(repository)
+        self.calculate_metrics_use_case = CalculateMetrics(repository)
 
-    def adicionar_forma_geometrica(self, forma_geometrica):
-        self.__repository.add(forma_geometrica)
+    def adicionar_forma_geometrica(self, tipo_forma):
+        self.create_shape_use_case.execute(tipo_forma)
 
     def listar_formas_geometricas(self):
-        formas = self.__repository.list_all()
-        if not formas:
-            print("Nenhuma forma geométrica cadastrada.")
-            return
-
-        print("### Formas Geométricas ###")
-        for i, forma in enumerate(formas, start=1):
-            print(f"{i}. {forma}")
+        return self.create_shape_use_case.repository.list_all()
 
     def calcular_area(self, index):
-        forma = self.__repository.get(index)
-        if forma:
-            return forma.calcular_area()
+        return self.calculate_metrics_use_case.calcular_area(index)
 
     def calcular_perimetro(self, index):
-        forma = self.__repository.get(index)
-        if forma:
-            return forma.calcular_perimetro()
+        return self.calculate_metrics_use_case.calcular_perimetro(index)
 
     def distancia_origem(self, index):
-        forma = self.__repository.get(index)
-        if forma:
-            return forma.distancia_origem()
+        return self.calculate_metrics_use_case.distancia_origem(index)
 
     def distancia_pontos(self, index, ponto):
-        forma = self.__repository.get(index)
-        if forma:
-            return forma.distancia_pontos(ponto)
+        return self.calculate_metrics_use_case.distancia_pontos(index, ponto)
 
     def contem_ponto(self, index, ponto):
-        forma = self.__repository.get(index)
-        if forma:
-            return forma.contem_ponto(ponto)
+        return self.calculate_metrics_use_case.contem_ponto(index, ponto)
 
-    def mover_formas(self, index, novo_ponto):
-        forma = self.__repository.get(index)
-        if forma:
-            return forma.mover(novo_ponto)
+    def mover_forma(self, index, novo_ponto):
+        self.move_shape_use_case.execute(index, novo_ponto)
 
     def mover_segmento_de_reta(self, index, novo_ponto1, novo_ponto2):
-        forma = self.__repository.get(index)
-        if forma:
-            return forma.mover(novo_ponto1, novo_ponto2)
+        self.move_shape_use_case.execute(index, novo_ponto1, novo_ponto2)
 
     def adicionar_ponto(self):
-        ponto = Point.criar_ponto()
-        self.adicionar_forma_geometrica(ponto)
+        self.adicionar_forma_geometrica("ponto")
 
     def adicionar_segmento_de_reta(self):
-        reta = LineSegment.criar_segmento_de_reta()
-        self.adicionar_forma_geometrica(reta)
+        self.adicionar_forma_geometrica("segmento_de_reta")
 
     def adicionar_circulo(self):
-        circulo = Circle.criar_circulo()
-        self.adicionar_forma_geometrica(circulo)
+        self.adicionar_forma_geometrica("circulo")
 
     def adicionar_retangulo(self):
-        retangulo = Rectangle.criar_retangulo()
-        self.adicionar_forma_geometrica(retangulo)
+        self.adicionar_forma_geometrica("retangulo")
 
     def adicionar_triangulo(self):
-        triangulo = Triangle.criar_triangulo()
-        self.adicionar_forma_geometrica(triangulo)
+        self.adicionar_forma_geometrica("triangulo")
